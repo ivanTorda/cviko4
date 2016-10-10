ce6ab1a980a5af48165e99a702078d0c3c904035
ivanTorda <ivan.torda@gmail.com>
2016-10-10 16:23:52 +0200

    cv4, uloha1, inicializacia ADC prevodnika

---
+++ src/main.c
@@ -0,0 +1,170 @@
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  MCD Application Team
+ * @version V1.0.0
+ * @date    11-February-2014
+ * @brief   Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+ *
+ * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *        http://www.st.com/software_license_agreement_liberty_v2
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup IO_Toggle
+ * @{
+ */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static __IO uint32_t TimingDelay;
+uint8_t BlinkSpeed = 0;
+/* Private function prototypes -----------------------------------------------*/
+RCC_ClocksTypeDef RCC_Clocks;
+/* Private functions ---------------------------------------------------------*/
+
+void adc_init(void);
+/**
+ * @brief   Main program
+ * @param  None
+ * @retval None
+ */
+int main(void) {
+	/*!< At this stage the microcontroller clock setting is already configured,
+	 this is done through SystemInit() function which is called from startup
+	 file (startup_stm32l1xx_xl.s) before to branch to application main.
+	 To reconfigure the default setting of SystemInit() function, refer to
+	 system_stm32l1xx.c file
+	 */
+
+	/* SysTick end of count event each 1ms */
+	RCC_GetClocksFreq(&RCC_Clocks);
+	SysTick_Config(RCC_Clocks.HCLK_Frequency / 1000);
+
+	/* Initialize LED2 */
+	STM_EVAL_LEDInit(LED2);
+
+	/* Initialize User_Button on STM32NUCLEO */
+	STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_EXTI);
+
+	/* Initiate Blink Speed variable */
+	BlinkSpeed = 0;
+	adc_init();
+	/* Start ADC Software Conversion */
+
+	uint16_t AD_value;
+	while (1) {
+		ADC_SoftwareStartConv(ADC1);
+		while (!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC)) {
+		}
+		AD_value = ADC_GetConversionValue(ADC1);
+
+	}
+}
+
+void adc_init(void) {
+	GPIO_InitTypeDef GPIO_InitStructure;
+	ADC_InitTypeDef ADC_InitStructure;
+	/* Enable GPIO clock */
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE); //Opravit a upravit
+	/* Configure ADCx Channel 2 as analog input */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	/* Enable the HSI oscillator */
+	RCC_HSICmd(ENABLE);
+	/* Check that HSI oscillator is ready */
+	while (RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET)
+		;
+	/* Enable ADC clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	/* Initialize ADC structure */
+	ADC_StructInit(&ADC_InitStructure);
+	/* ADC1 configuration */
+	ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+	ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+	ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_NbrOfConversion = 1;
+	ADC_Init(ADC1, &ADC_InitStructure);
+	/* ADCx regular channel8 configuration */
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_16Cycles);
+	/* Enable the ADC */
+	ADC_Cmd(ADC1, ENABLE);
+	/* Wait until the ADC1 is ready */
+	while (ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == RESET) {
+	}
+	/* Start ADC Software Conversion */
+	ADC_SoftwareStartConv(ADC1);
+
+}
+
+/**
+ * @brief  Inserts a delay time.
+ * @param  nTime: specifies the delay time length, in 1 ms.
+ * @retval None
+ */
+void Delay(__IO uint32_t nTime) {
+	TimingDelay = nTime;
+
+	while (TimingDelay != 0)
+		;
+}
+
+/**
+ * @brief  Decrements the TimingDelay variable.
+ * @param  None
+ * @retval None
+ */
+void TimingDelay_Decrement(void) {
+	if (TimingDelay != 0x00) {
+		TimingDelay--;
+	}
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+	/* User can add his own implementation to report the file name and line number,
+	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+	/* Infinite loop */
+	while (1)
+	{
+	}
+}
+#endif
+
+/**
+ * @}
+ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
